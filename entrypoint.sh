f80bf1f2064f1ea7fc32009e09814fbbb78b8cc7
forwardmyapps0 <forwardmyapps@proton.me>
2026-06-04 21:10:17 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,32 +1,28 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -e
 
-# Passwort generieren, falls keines über Umgebungsvariable übergeben wurde
-if [ -z "$MY_PASS" ]; then
-    MY_PASS=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 16)
+cd /opt/webvirtcloud
+
+if [ ! -f "webvirtcloud/settings.py" ]; then
+  cp webvirtcloud/settings.py.template webvirtcloud/settings.py
+  
+  # Generierung eines sicheren Keys
+  SECRET=$(head /dev/urandom | tr -dc 'a-zA-Z0-9' | head -c 128)
+  sed -r "s/SECRET_KEY = .*$/SECRET_KEY = '${SECRET}'/" -i webvirtcloud/settings.py
+  
+  # Migrationen ausführen
+  python3 manage.py migrate
+
+  # SSH Keys generieren
+  ssh-keygen -b 4096 -t rsa -f /root/.ssh/id_rsa -q -N ""
+  ssh-keygen -t ed25519 -f /root/.ssh/id_ed25519 -q -N ""
+
+  echo "RSA ----------------------------"
+  cat /root/.ssh/id_rsa.pub
+  echo "---------------------------- RSA"
+  echo "ED25519 ------------------------"
+  cat /root/.ssh/id_ed25519.pub
+  echo "------------------------ ED25519"
 fi
 
-# Root Passwort setzen
-echo "root:$MY_PASS" | chpasswd
-
-# VNC Passwort setzen
-mkdir -p /root/.vnc
-echo "$MY_PASS" | vncpasswd -f > /root/.vnc/passwd
-chmod 600 /root/.vnc/passwd
-
-# Zertifikat für noVNC erstellen, falls nicht vorhanden
-if [ ! -f /root/self.pem ]; then
-    openssl req -new -subj '/C=DE/ST=None/L=None/O=None/CN=localhost' -x509 -days 365 -nodes -out /root/self.pem -keyout /root/self.pem
-fi
-
-# Dienste starten
-/usr/sbin/sshd
-vncserver -localhost no -SecurityTypes VncAuth -geometry 1024x768 :1
-websockify -D --web=/usr/share/novnc/ --cert=/root/self.pem 6080 localhost:5901
-
-echo "========================================="
-echo "ZUGANG: root / $MY_PASS"
-echo "========================================="
-
-# Container am Laufen halten
-tail -f /dev/null
+exec "$@"
